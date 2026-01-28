3fc5962c15d0b45bcaad41078bc3a251bbaa1aa3
Jim Toth <jim@artby.city>
2026-01-28 11:33:31 -0500

    Update root GET message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!'
+    return 'Formr Capture Service build & operated by Memetic Block, see https://memeticblock.com for more info'
   }
 }
